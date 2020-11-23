8485d5ee90170567e2a418fcabe741d78621a37d
Faaiz <faaizasm@khu.ac.kr>
2020-11-23 17:55:48 +0900

    --

--- main_binary.py
+++ main_binary.py
@@ -155,7 +155,7 @@ def main():
     criterion.type(args.type)
     model.type(args.type)
 
-    val_data = get_dataset(args.dataset, 'val', transform['eval'])
+    val_data = get_dataset(args.dataset + '/val', 'val', transform['eval'])
     val_loader = torch.utils.data.DataLoader(
         val_data,
         batch_size=args.batch_size, shuffle=False,
@@ -166,7 +166,7 @@ def main():
         print("Evaluation Results:\tLoss "+str(round(val_loss,4))+'\tPrec@1 '+str(round(val_prec1,4))+'\tPrec@5 '+str(round(val_prec5,4)))
         return
 
-    train_data = get_dataset(args.dataset , 'train', transform['train'])
+    train_data = get_dataset(args.dataset + '/train', 'train', transform['train'])
     train_loader = torch.utils.data.DataLoader(
         train_data,
         batch_size=args.batch_size, shuffle=True,
