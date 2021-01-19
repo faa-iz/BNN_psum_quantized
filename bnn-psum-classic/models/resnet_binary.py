b04e52e6a4d6fde38df4009d815a379c5b0315b5
Faaiz <faaizasm@khu.ac.kr>
2021-01-19 17:36:33 +0900

    a

--- bnn-psum-classic/models/resnet_binary.py
+++ bnn-psum-classic/models/resnet_binary.py
@@ -445,8 +445,9 @@ class BasicBlock(nn.Module):
             if quantize:
                 out_tensor = custom_quantize(out_tensor, num_bit)
             elif binarize:
-                out_tensor = self.tanh1(out_tensor)
+                #out_tensor = self.tanh1(out_tensor)
                 scale = out_tensor.abs().mean()
+                out_tensor = scale * self.tanh2(out_tensor)
                 out_tensor = scale * Binarize(out_tensor)
 
 
@@ -564,8 +565,9 @@ class BasicBlock(nn.Module):
             if quantize:
                 out_tensor = custom_quantize(out_tensor, num_bit)
             elif binarize:
-                out_tensor = self.tanh2(out_tensor)
+
                 scale = out_tensor.abs().mean()
+                out_tensor = scale*self.tanh2(out_tensor)
                 out_tensor = scale * Binarize(out_tensor)
 
             output = output + out_tensor
